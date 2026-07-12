a783c5be1c86a1f1aa2b9cb83d7b918646dcb6fd
ariella.litwackgmail.com <ariella.litwack@gmail.com>
2026-07-12 13:22:44 +0300

    Index problem solving

---
+++ server/index.ts
@@ -0,0 +1,64 @@
+import express, { type Express, type Request, type Response } from 'express';
+
+const app: Express = express();
+const port = 3000;
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+
+
+
+/*
+
+
+let notes = [
+  {
+    id: '0',
+    name: 'John',
+    number: '123-456-7890'
+  }
+]
+
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+import express, { type Express, type Request, type Response } from 'express';
+import { Pool } from 'pg'
+
+const app: Express = express();
+const port = 5432;
+const pool = new Pool({
+  user: 'postgres',
+  host: 'Civil-Sketch',
+  database: 'Postgres',
+  password: '5432',
+  port: 5432,
+});
+
+app.get('/', (req: Request, res: Response) => {
+  pool.query('SELECT NOW()');
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+*/
