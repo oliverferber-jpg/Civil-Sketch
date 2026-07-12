1d004a1ac8cc9dbc3101f5313f518b7865256efb
ariella.litwackgmail.com <ariella.litwack@gmail.com>
2026-07-12 14:35:28 +0300

    index test

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,13 @@
 import express, { type Express, type Request, type Response } from 'express';
 
-
-const app: express = express();
-const port = 3001;
+const app: Express = express();
+const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send("Hiasdfld");
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
-
   console.log(`Example app listening on port ${port}`);
 });
 
